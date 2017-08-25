a9e94672c0d337f2eb950e3ebc60863817f28554
Chris Richard <christopher.richard27@gmail.com>
2017-08-24 21:28:15 -0700

    modified systemconfig and td-agent

---
+++ cookbooks/td-agent/recipes/default.rb
@@ -0,0 +1,50 @@
+#
+# Cookbook Name:: td-agent
+# Recipe:: default
+#
+# Copyright 2011, Treasure Data, Inc.
+#
+
+Chef::Recipe.send(:include, TdAgent::Version)
+Chef::Provider.send(:include, TdAgent::Version)
+
+include_recipe 'td-agent::install'
+
+reload_action = (reload_available?) ? :reload : :restart
+
+major_version = major
+template "/etc/td-agent/td-agent.conf" do
+  owner  node["td_agent"]["user"]
+  group  node["td_agent"]["group"]
+  mode "0644"
+  cookbook node['td_agent']['template_cookbook']
+  source "td-agent.conf.erb"
+  variables(
+    :major_version => major_version
+  )
+  notifies reload_action, "service[td-agent]", :delayed
+end
+
+node["td_agent"]["plugins"].each do |plugin|
+  if plugin.is_a?(Hash)
+    plugin_name, plugin_attributes = plugin.first
+    td_agent_gem plugin_name do
+      plugin true
+      %w{action version source options gem_binary}.each do |attr|
+        send(attr, plugin_attributes[attr]) if plugin_attributes[attr]
+      end
+    end
+  elsif plugin.is_a?(String)
+    td_agent_gem plugin do
+      plugin true
+    end
+  end
+end
+
+service "td-agent" do
+  supports :restart => true, :reload => (reload_action == :reload), :status => true
+  action [ :enable, :start ]
+end
+
+##### /var/log/td-agent
+##### /var/log/td-agent/buffer
